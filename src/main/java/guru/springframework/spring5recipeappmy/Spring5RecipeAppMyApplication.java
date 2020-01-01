dfcf659ec0ba2f35ff48ee18de11ff29e5e65f14
kitamarekweb <kitamarekweb@gmail.com>
2020-01-01 20:46:43 +0100

    t-107. creating index page

---
+++ src/main/java/guru/springframework/spring5recipeappmy/Spring5RecipeAppMyApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5recipeappmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeAppMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipeAppMyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring5recipeappmy/Spring5RecipeAppMyApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5recipeappmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppMyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
