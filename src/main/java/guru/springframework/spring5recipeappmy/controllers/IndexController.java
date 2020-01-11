a4cee648f58bc5e66e069bf15b9f9fbd06232769
kitamarekweb <kitamarekweb@gmail.com>
2020-01-11 19:29:18 +0100

    t-172. code refactor with project LOMBOK. Invalidate Caches/Restart.

--- src/main/java/guru/springframework/spring5recipeappmy/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeappmy/controllers/IndexController.java
@@ -5,12 +5,14 @@ import guru.springframework.spring5recipeappmy.domain.UnitOfMeasure;
 import guru.springframework.spring5recipeappmy.repositories.CategoryRepository;
 import guru.springframework.spring5recipeappmy.repositories.UnitOfMeasureReposiotory;
 import guru.springframework.spring5recipeappmy.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -30,6 +32,7 @@ public class IndexController {
 
     @RequestMapping({"","/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting index page");
 
 //        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
 //        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureReposiotory.findByDescription("Teaspoon");
